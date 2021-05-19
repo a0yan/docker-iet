3886969bc7eb0f6fd7110b90eff55501aef7edaa
Ayan Shrivastava <ayan.shrivastava01@gmail.com>
2021-05-19 20:59:17 +0530

    Changing DB Credetials

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
 require('dotenv').config()
-const pg = require("pg")
-const Client=pg.Client
-const client = new Client({
-    connectionString:process.env.CONNECTION_STRING,
-    ssl: { rejectUnauthorized: false }
-  })
-  client.connect();
-module.exports = client;
+try {
+  const pg = require("pg")
+  const Client=pg.Client
+  const client = new Client({
+      connectionString:process.env.CONNECTION_STRING,
+      ssl: { rejectUnauthorized: false }
+    })
+    client.connect();
+    module.exports = client;  
+} catch (error) {
+  console.error(error.message)
+}
+
